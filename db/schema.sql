0397d279a190963910abdcb55cd9aa7632030d8d
JGJ <jordan.gb0@gmail.com>
2023-03-09 21:34:50 -0500

    add db code

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
 
 DROP DATABASE IF EXISTS friends_db;
 CREATE DATABASE friends_db;
+
+USE friends_db
+
+CREATE TABLE note_cards(
+id INT NOT NUll,
+first_name VARCHAR(30),
+note_description VARCHAR(30),
+note VARCHAR(30)
+);
