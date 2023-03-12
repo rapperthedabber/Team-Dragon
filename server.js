efccfce66fcf7f873743b0c634c5292270332b01
Jerry <jamesharold1738@gmail.com>
2023-03-11 22:38:59 -0500

    stuff

--- server.js
+++ server.js
@@ -30,16 +30,17 @@ const sess = {
 };
 
 app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
