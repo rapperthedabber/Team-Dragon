91439f1f642a36dd7641b1254597d2bd4be581a7
JGJ <jordan.gb0@gmail.com>
2023-03-11 22:30:37 -0500

    copied seeds.js from mini project-editiedschema.sql

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,34 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+// const seedCategories = require('./category-seeds');
+// const seedProducts = require('./product-seeds');
+// const seedTags = require('./tag-seeds');
+// const seedProductTags = require('./product-tag-seeds');
+
+// const sequelize = require('../config/connection');
+
+// Question if this is on an index file or a Seeds.js file like in the mini project
 
 const sequelize = require('../config/connection');
+const { User, Project } = require('../models');
+
+const userData = require('./userData.json');
+const projectData = require('./projectData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
