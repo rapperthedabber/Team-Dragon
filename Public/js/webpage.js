8536f1fb4ce611a5e4579325a9716daca75b8aab
Jerry <jamesharold1738@gmail.com>
2023-03-16 19:29:11 -0400

    added API to javascript

--- Public/js/webpage.js
+++ Public/js/webpage.js
@@ -4,6 +4,8 @@ const profile1 = document.getElementById('profile1');
 const profile2 = document.getElementById('profile2');
 const description1 = document.getElementById('description1');
 const description2 = document.getElementById('description2');
+const description1Half = document.getElementById('description1.5')
+const description2Half = document.getElementById('description2.5')
 
 function seeProfiles() {
   const url = 'https://animechan.vercel.app/api/random';
@@ -11,8 +13,16 @@ function seeProfiles() {
     .then((data) => data.json())
     .then((data) => {
       console.log(data);
+      const anime = data.anime
+      const quote = data.quote
+      description1.textContent = JSON.stringify(anime);
+      description1Half.textContent = JSON.stringify(quote);
+      description2.textContent = JSON.stringify(anime);
+      description2Half.textContent = JSON.stringify(quote)
+      
+    
+      
     });
-    fetch 
 }
 
 seeProfiles();
